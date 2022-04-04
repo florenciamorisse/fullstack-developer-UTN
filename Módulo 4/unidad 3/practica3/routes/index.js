e11ce195ef648e9a9508a75083a85bb4279b2bad
Florencia Morisse <florenciamorisse@gmail.com>
2022-04-04 13:43:10 -0300

    terminé ejercicio express

--- Módulo 4/unidad 3/practica3/routes/index.js
+++ Módulo 4/unidad 3/practica3/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Florencia' });
+  res.render('index', { title: '2022' });
 });
 
 module.exports = router;
