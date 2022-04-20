2c233952791ee11781fa1589fde70bfe3e4d2592
Florencia Morisse <florenciamorisse@gmail.com>
2022-04-20 10:11:49 -0300

    row to rows

--- Módulo 4/backend/models/usuariosModel.js
+++ Módulo 4/backend/models/usuariosModel.js
@@ -5,7 +5,7 @@ async function getUserByUsernameAndPassword(user, password){
     try{
         var query = 'select * from usuarios where usuario =? and password = ? limit 1';
         var rows = await pool.query(query,[user, md5(password)]);
-        return row[0];
+        return rows[0];
     }catch(error){
         console.log(error);
     }
