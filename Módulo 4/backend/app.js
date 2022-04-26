20f1aa049840d67cb3225e14d17f10611f766ee8
Florencia Morisse <florenciamorisse@gmail.com>
2022-04-26 12:05:06 -0300

    arreglando el backend

--- Módulo 4/backend/app.js
+++ Módulo 4/backend/app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 require('dotenv').config();
+var session=require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -23,10 +24,32 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  secret:'floryseba',
+  cookie: {maxAge:null},
+  resave:false,
+  saveUninitialized: true
+}))
+
+secured=async(req, res, next)=>{
+  try{
+    console.log(req.session.id_usuario);
+    if(req.session.id_usuario){
+      next();    
+    }else{
+      res.redirect('/admin/login');      
+    }
+  }catch(error){
+    console.log(error);
+  }
+}
+ 
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin/login', loginRouter);
-app.use('/admin/novedades', adminRouter);
+app.use('/admin/novedades',secured, adminRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
