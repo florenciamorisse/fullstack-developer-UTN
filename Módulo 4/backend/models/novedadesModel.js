414ff14b68673320df168a9e853acbc6b06215c2
Florencia Morisse <florenciamorisse@gmail.com>
2022-04-27 12:39:07 -0300

    insert

--- Módulo 4/backend/models/novedadesModel.js
+++ Módulo 4/backend/models/novedadesModel.js
@@ -5,6 +5,16 @@ async function getNovedades(){
         var rows = await pool.query(query); 
         return rows;   
 }
-
+async function insertNovedades(obj){
+    try{
+        var query = 'insert into novedades set?';
+        var rows = await pool.query(query,[obj]);
+        return rows;
+    }catch(error){
+        console.log(error);
+        throw error;
+        
+    }
+}
 
 module.exports={getNovedades}
