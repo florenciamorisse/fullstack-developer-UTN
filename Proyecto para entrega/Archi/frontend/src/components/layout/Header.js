e6433574e2e61762b22989b4ec0aa13f78c24853
Florencia Morisse <florenciamorisse@gmail.com>
2022-05-12 13:27:37 -0300

    imagenes

--- Proyecto para entrega/Archi/frontend/src/components/layout/Header.js
+++ Proyecto para entrega/Archi/frontend/src/components/layout/Header.js
@@ -2,9 +2,35 @@ import '../../styles/components/layout/Header.css'
 
 const Header =(props)=>{
     return(
-      <header>
-      Lorem
-      </header>
+        <header className="header">        
+            <nav className="nav">
+                <a href="#" className="logo">
+                    <img className="logo" src="img/logo3.png" />
+                </a>
+            <button className="nav-toggle">
+                <i className="fa-solid fa-bars"></i>
+            </button>
+            <ul className="nav-menu ">
+                <li className="nav-menu-item">
+                    <a className="nav-menu-link nav-link active" href="index.html">Home</a>
+                </li>
+                <li clasNames="nav-menu-item">
+                    <a clasNames="nav-menu-link nav-link" href="BIM.html">BIM</a>
+                </li>
+                <li className="nav-menu-item">
+                    <a className="nav-menu-link nav-link" href="planos.html">Planos</a>
+                </li>
+                <li className="nav-menu-item">
+                    <a className="nav-menu-link nav-link" href="reformas.html">Reformas</a>
+                </li>                
+                <li className="nav-menu-item">
+                    <a className="nav-menu-link nav-link" href="contacto.html">Contacto</a>
+                </li>
+            </ul>
+        </nav>  
+           
+        
+    </header>
     );
 }
 export default Header;
